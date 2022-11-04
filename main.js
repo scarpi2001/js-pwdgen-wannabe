3ffd552e9bdd929400d50c402612b61b0e6a719b
Marco <posta.email3001@gmail.com>
2022-11-04 15:31:02 +0100

    setup_html_css_js

---
+++ main.js
@@ -0,0 +1,12 @@
+//generare una password cheidendo all'utente: nome, cognome e colore preferito
+
+//variabili
+
+
+//chiedere all'utente nome
+
+//chiedere all'utente cognome
+
+//chiedere all'utente colore preferito
+
+//output
